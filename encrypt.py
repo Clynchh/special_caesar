8826fee3e079532c8919d8187e7ad896b5d30e0d
Corey Lynch <coreylynch03@gmail.com>
2021-02-03 15:39:13 +0000

    finished with encryption

--- encrypt.py
+++ encrypt.py
@@ -1,19 +1,56 @@
-from test import reverse_word
+from re import sub
+from random import randint
 
-plaintext = input("Enter plaintext: ")
-plaintext = plaintext.split()
-for i in range(len(plaintext)):
-    length = len(plaintext[i])
-    for j in plaintext[i]:
-        print(f"{plaintext[i]}, {plaintext[i][::-1]}")
+rand_punc = [".",",",":",";","?","!"]
 
-def find_halves(word):
-    if word %2 == 0:
-        even = True
-        first_half = ""
-        second_half = ""
-    else:
-        even = False
-        first_half = ""
-        middle = plaintext[len(plaintext)/2 + 1]
-        second_half = ""
+def caesar_encrypt(string,key,caesar_word=""):
+    for char in string:
+        if (65 <= ord(char) <= 90):
+            char = char.lower()
+            char = chr((ord(char) + key - 97) % 26 + 97)
+            caesar_word += char
+        elif (97 <= ord(char) <= 122):
+            char = chr((ord(char) + key - 97) % 26 + 97)
+            caesar_word += char
+        else:
+            caesar_word += char
+    return caesar_word
+
+
+def reverse_word(word):    
+    if len(word) <= 3:
+        end_word = word[::-1]
+    elif len(word) % 2 == 0:
+        first_half = word[0:int((len(word)/2))]
+        second_half = word[int((len(word)/2)):int(len(word))]
+        first_half = first_half[::-1]
+        second_half = second_half[::-1]
+        end_word = first_half + second_half
+    elif len(word) % 2 != 0:
+        first_half = word[0:int((len(word)/2))]
+        middle = word[int(len(word)/2)]
+        second_half = word[int((len(word)/2) + 1): int(len(word))]
+        first_half = first_half[::-1]
+        second_half = second_half[::-1]
+        end_word = first_half + middle + second_half
+    
+    end_word += rand_punc[randint(0,5)]
+    return end_word
+
+
+key = int(input("key: "))
+message = input("message: ")
+message = message = sub(r"[^\w\s]", "",message)
+
+
+list_message = message.split()
+output_str = ""
+for word in list_message:
+    encrypted_word = reverse_word(word)
+    output_str += encrypted_word
+
+
+final_out = caesar_encrypt(output_str, key)
+print(final_out)
+
+
--- test.py
+++ test.py
@@ -1,13 +1,1 @@
-def reverse_word(word):    
-    if len(word) %2 == 0:
-        even = True
-        first_half = word[0:int((len(word)/2))]
-        second_half = word[int((len(word)/2)):int(len(word))]
-        print(f"{first_half}, {second_half}")
-    else:
-        even = False
-        first_half = word[0:int((len(word)/2))]
-        middle = word[int((len(word)/2) + 1)]
-        second_half = word[int((len(word)/2) + 1): int(len(word))]
-        print(f"{first_half}, {middle}, {second_half}")
-    
+Hello, How are you?
